f99c2d927b0157566c0379a4b401d2ea35d2ebe8
felix goff <felix.goff@edu.lomma.se>
2023-11-14 16:22:47 +0100

    l

--- ff.sh
+++ ff.sh
@@ -1,3 +1,6 @@
+cd ./files
 wget https://cloud.oscie.net/acdp/acnh/$1/$2.mp3
-mv ./$2.mp3 ./main.mp3
+cd ..
+mv ./files/$2.mp3 ./main.mp3
+rm ./files/*
 ffmpeg -i ./main.mp3 -listen 1 -method GET -c copy -f MP3 http://0.0.0.0:5000/main.mp3
